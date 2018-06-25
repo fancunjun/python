86a8390942ccceeb61984fb5349185db176cba7a
fancunjun <417592877@qq.com>
2018-06-25 21:15:06 +0800

    This is numer and Mathematical calculations

---
+++ git/ex3.py
@@ -0,0 +1,38 @@
+#--coding:utf-8--
+print "I will now count my chickens:"
+#先计算除法，再计算加法
+print "Hens ",25 + 30 / 6 
+#计算乘法，取模运算，再计算减法
+print "Roosters", 100 -25 * 3 % 4
+#输入打印信息
+print "Now I will count the eggs:"
+#计算模运算，除法，其他从左到右计算
+print 3+2 + 1 - 5 + 4 % 2 -1 / 4 + 6
+#输出打印信息
+print "Is it true that 3 + 2 < 5 - 7?"
+#判断3+3是否小于5-7，返回布尔值
+print 3 + 2 < 5 -7
+#计算3+2
+print "What is 3 + 2",3 + 2
+#计算5-7
+print "What is 5 -7?",5-7
+#输出打印信息
+print "Oh,that's why it's False."
+#输出打印信息
+print "How about some more."
+#判断5>-2为真为假
+
+print "Is it greater?", 5 > -2 
+#判断5>=-2为真为假
+
+print "Is it greater or equal?" , 5 >= -2
+#判断5<=2 为真为候
+
+print "Is it less or equal?" ,5 <= -2
+
+#test03 count house value
+
+print 10798 * 49 - 10798 * 49 * 0.25
+
+#floating point number
+print 10798.34 * 49.45 - 10000.23 /20.45
